8e24611349fcd3b5a9385c162eab6a0cd46bf997
Baby <baby@gmail.com>
2024-01-11 22:33:17 +0000

    first commit

---
+++ User-Search-App/src/App.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'  
+import './App.css'
+import Card from './components/Card'
+import Header from './components/Header'
+import Search from './components/Search'
+import axios from 'axios'
+
+function App() {
+  const [githubUser, setGithubUser] = useState(null)
+  const [loading, setLoading]= useState(false)
+  const [error,setError] = useState(null)
+
+  const getGithubUser = async (githubUser) =>{
+    try {
+      setLoading(true)
+      const response = await axios.get(`https://api.github.com/users/${githubUser}`, {
+        headers: {
+          Authorization:`Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`
+        }
+      })
+
+      const data = await response.data
+      console.log(data)
+      setLoading(false)
+      setGithubUser(data)
+
+    } catch (error) {
+        console.log(error)
+        setError('No Results',error)
+      }
+}
+
+
+  return (
+  
+    <div className='w-100 h-screen bg-gray-light dark:bg-tetiaryBlack ' >
+      <div className='w:[90%] md:w-[60%] lg:w-[45%] m-auto pt-[10rem] flex flex-col gap-5 overflow-hidden'>
+        <Header/>
+        <Search getGithubUser ={getGithubUser} error={error}/>
+        <Card githubUser= {githubUser}/>
+      </div>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ User-Search-App/src/components/Search.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import searchIcon from '../assets/Search-icon.svg'
+import { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContextProvider'
+import { useState } from 'react'
+
+
+
+const Search = ({getGithubUser, error}) => {
+  const {theme} = useContext(ThemeContext)
+  const [query, setQuery] = useState('')
+
+  const handleSubmit = () =>{
+    if (!query){
+      alert ('Field is required')
+    }else {
+      getGithubUser(query)
+      setQuery('')
+    }
+  }
+
+  return (
+    <div className='p-2 bg-white flex justify-between items-center shadow rounded-lg dark:bg-blueBlack' >
+      <div className='flex gap-3 ml-2'>
+        <img src={searchIcon} alt='search icon' className='w-[24px] h-[24px]'/>
+        <input  value={query} onChange={(e) => setQuery(e.target.value)} className='border-0 outline-none placeholder:text-gray-200 w-[390px] text-[16px] dark:bg-transparent dark:placeholder:text-white' type='text' placeholder='Search Github username...' />
+      </div> 
+      <div className='flex space-x'>
+        <div className='text-red-500 items-center w-20 mt-2 mr-3'>{error}</div>
+        <button onClick={handleSubmit} className='px-5 py-2 flex justify-center items-center text-white bg-primaryBlue rounded hover:bg-[#60ABFF] text- [13px] font-medium'>Submit</button>
+      </div>
+    </div>
+  )
+}
+
+export default Search 
